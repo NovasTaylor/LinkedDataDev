8ee0683657c460191bae535129708d0555f9344b
NovasTaylor <NovasTaylor@gmail.com>
2017-12-06 11:06:45 -0500

    Node type (class)  updates with changes from the edit window

--- GraphEditor/graphEditorV2.js
+++ GraphEditor/graphEditorV2.js
@@ -52,29 +52,34 @@ var w = 900,
 var svg = d3.select("#whiteboard").append("svg")
   .attr("width", w)
   .attr("height", h);
-// Build the arrow for RDF Triple
+
+/* Arrow for Link
+   Must have id:String for display
+   refX: increase to back the arrow away from the target node
+   markerHeight: Arrow height
+   markerWidth: Arrow Width
+*/
 svg.append("defs")
   .selectAll("marker")
   .data(["end"])      // Different link/path types can be defined here
-  .enter().append("marker")    // This section adds in the arrows
-  .attr("id", String)
-  .attr("viewBox", "0 -5 10 10")
-  .attr("refX", 5)  // Increase this number to back the arrow away from the target node
-  .attr("refY", 0)
-  .attr("fill", "#756bb1")
-  .attr("stroke", "white")
-  .attr("markerWidth", 2.5)  // Arrow height
-  .attr("markerHeight", 2.5) // Arrow width
-  .attr("orient", "auto")
+  .enter().append("marker")    // Append arrow marker
+  .attr({'id':String,
+    'viewBox': '0 -5 10 10',
+    'refX':5,
+    'refY': 0,
+    'fill':'black',
+    'stroke':'white',
+    'markerWidth':2.5,
+    'markerHeight': 2.5,
+    'orient':'auto'
+  })
   .append("svg:path")
   .attr("d", "M0,-5L10,0L0,5");
 
+// Initialize D3 force layout
 var force = d3.layout.force()
   .nodes(nodes)
   .links(edges)
-  .gravity(.05)
-  .charge(-180)
-  .linkDistance(100)
   .size([w, h])
   .start();
 
@@ -93,46 +98,8 @@ var force = d3.layout.force()
   linkTextBack.append('feComposite')
       .attr("in", "SourceGraphic");
 
-//TW NEW INSERTION -------------------------------------------
-// Define arrow markers for graph links
-svg.append('svg:defs').append('svg:marker')
-  .attr('id', 'end-arrow')
-  .attr('viewBox', '0 -5 10 10')
-  .attr('refX', 6)
-  .attr('markerWidth', 3)
-  .attr('markerHeight', 3)
-  .attr('orient', 'auto')
-  .append('svg:path')
-    .attr('d', 'M0,-5L10,0L0,5')
-    .attr('fill', '#000');
-svg.append('svg:defs').append('svg:marker')
-  .attr('id', 'start-arrow')
-  .attr('viewBox', '0 -5 10 10')
-  .attr('refX', 4)
-  .attr('markerWidth', 3)
-  .attr('markerHeight', 3)
-  .attr('orient', 'auto')
-  .append('svg:path')
-    .attr('d', 'M10,-5L0,0L10,5')
-    .attr('fill', '#000');
-
-// Line displayed when dragging new nodes
-var drag_line = svg.append('svg:path')
-  .attr('class', 'link dragline hidden')
-  .attr('d', 'M0,0L0,0');
-
-// Handles to link and node element groups
-var path     = svg.append('svg:g').selectAll('path'),
-  linkText = svg.append('svg:g').selectAll('textPath'),  // Group for edge labels
-  circle   = svg.append('svg:g').selectAll('g');
-
-
-//TW END NEW INSERTION ---------------------------------------
-
-
 var drag = force.drag()
   .on("dragstart", dragstart);
-
 
 // Relationship lines
 var links = svg.selectAll("reLine")
@@ -160,6 +127,7 @@ var circles = svg.selectAll("g.node")
 
 circles.append("circle")
   .attr("r", 50)
+  .attr("id", function(d, i) {return("circle"+i) ; })  // ID used to update class
   .attr("class", function(d,i){
     if (d.type == "STRING"){ return "string";}
     else if (d.type == "URI"){ return "uri"; }
@@ -173,8 +141,8 @@ circles.append("circle")
   .on('mouseout', function(d){
     var nodeSelection= d3.select(this).style({opacity:'1.0',})
   })
-//---- Double CLICK NODE TO EDIT ---------------------------------------------------//
-  // .on("click", function(d, i){
+  //---- Double CLICK NODE TO EDIT ---------------------------------------------------//
+  // .on("click", function(d, i){  // was prev. single click
   .on("dblclick", function(d, i){
     console.log("clicked");
     var self = this;
@@ -182,13 +150,12 @@ circles.append("circle")
     if (infoActive == true) {
       // clicked a node while previous info block displayed
       d3.selectAll("input").remove();
-      //d3.selectAll(".formEle").remove();
       d3.select("#info").selectAll("*").remove();
       d3.select("#info").style("opacity", 0);
     }
-    d3.select("#info").style("opacity", 1);
+    d3.select("#info").style("opacity", 1);  // Display edit div
 
-    var div = d3.select("#info")
+    var div = d3.select("#info")  // Selet div for appending
 
     var labelText = div.append("p")
       .text("Label: ");
@@ -197,16 +164,16 @@ circles.append("circle")
       .attr("type", "text")
       .attr("value", d.label);
 
-      var prefixText = div.append("p")
+    var prefixText = div.append("p")
         .text("Prefix: ");
-      var prefixInput = prefixText.append("input")
+    var prefixInput = prefixText.append("input")
         .attr("size", "15")
         .attr("type", "text")
         .attr("value", d.prefix);
 
-        var typeText = div.append("p")
+    var typeText = div.append("p")
           .text("Type: ");
-        var typeInput = typeText.append("input")
+    var typeInput = typeText.append("input")
           .attr("size", "15")
           .attr("type", "text")
           .attr("value", d.type);
@@ -219,23 +186,32 @@ circles.append("circle")
             .text(function(d) {
               return (d.label = labelInput.node().value); })
           ;
+          // Change class of circle to match TYPE
+          d3.select("#circle" + i)
+             //Hang head in shame for this horrible kludge. Make this smarter.
+             //  detect exist class.If changed: Remove existing, update to new
+            .classed("string", false)  // remove the class
+            .classed("uri", false)  // remove the class
+            .classed(typeInput.node().value.toLowerCase(), true)
+              ;
           d3.select("#prefixText" + i)
             .text(function(d) {
               return (d.prefix = prefixInput.node().value); })
           ;
-          d3.select("#typeText" + i)
+          var foo = d3.select("#typeText" + i)
             .text(function(d) {
               return (d.type = typeInput.node().value); })
+
+          //   .attr("class", function(d) {
+          //    return ( toLowerCase(d.type = typeInput.node().value)); } )
           ;
-
-
           // Clean up the info window
           d3.select("#info").selectAll("*").remove();
           d3.select("#info").style("opacity", 0);
         })
 
     infoActive = true;
-  });
+  }); // End of dblClick
 //---- END NODE CLICK -------------------------------------------------------//
 // dx sets how close to the node the label appears
 circles.append("text")
