0907b3f118fcec25596dc6b32f43dad809a39df3
NovasTaylor <NovasTaylor@gmail.com>
2017-12-11 14:50:15 -0500

    runs. WIP reformat

--- GraphEditor/graphEditor.js
+++ GraphEditor/graphEditor.js
@@ -79,38 +79,147 @@ var svg = d3.select("#whiteboard").append("svg")
   .attr("width", w)
   .attr("height", h);
 
-/* Arrow for Link
-   Must have id:String for display
-   refX: increase to back the arrow away from the target node
-   markerHeight: Arrow height
-   markerWidth: Arrow Width
-*/
-svg.append('defs')
-  .selectAll('marker')
-  .data(["arrowhead"])      // Different link/path types can be defined here
-  .enter().append("marker")    // Append arrow marker
-  //TODO Move size and colouring to CSS
-  .attr({'id':String,
-    'viewBox': '0 -5 10 10',
-    'refX':    nodeRadius+25,
-    'refY':    0,
-    'fill':    'black',
-    'stroke':  'white',
-    'markerWidth':  2.5,
-    'markerHeight': 2.5,
-    'orient': 'auto'
-  })
-  .append("svg:path")
-    .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-    .attr('fill', 'black')
-    .attr('stroke','black');  // move to CSS
-
 // Initialize D3 force layout
 var force = d3.layout.force()
   .nodes(nodesData)
   .links(edgesData)
   .size([w, h])
   .start();
+
+// Edgess
+var edges = svg.selectAll("line")
+  .data(edgesData)
+  .enter()
+  .append("line")
+    .attr({
+      'id': function(d,i){return 'edge'+i},
+      'class': 'edge',
+      'marker-end': 'url(#arrowhead)'
+  })
+  .on('mouseover', function(d){
+    var nodeSelection = d3.select(this).style({opacity:'0.5'});
+  })
+  .on('mouseout', function(d){
+    var nodeSelection= d3.select(this).style({opacity:'1.0',})
+  });
+
+var nodes = svg.selectAll("g.node")
+  .data(nodesData)
+  .enter()
+  .append("g")
+    .attr("class", "node")
+    //.on("dblclick", dblclick)
+    // .call(drag);
+    .call(force.drag);
+
+    nodes.append("circle")
+      .attr("r", nodeRadius)
+      .attr("id", function(d, i) {return("circle"+i) ; })  // ID used to update class
+      .attr("class", function(d,i){
+        if (d.type == "STRING"){ return "string";}
+        else if (d.type == "URI"){ return "uri"; }
+        else {return "unspec";}
+      })
+
+      // Mousover Node - highlight node by fading the node colour during mouseover
+      .on('mouseover', function(d){
+        //var nodeSelection = d3.select(this).style({opacity:'0.5'});
+        var nodeSelection = d3.select(this).attr({'r':nodeRadius+5});
+      })
+
+      //Mouseout Node  - bring node back to full colour
+      .on('mouseout', function(d){
+        //  var nodeSelection= d3.select(this).style({opacity:'1.0',})
+        var nodeSelection = d3.select(this).attr({'r':nodeRadius});
+      })
+      //---- Double CLICK NODE TO EDIT ---------------------------------------------------//
+      // .on("click", function(d, i){  // was prev. single click
+      .on("dblclick", function(d, i){
+        console.log("clicked");
+        var self = this;
+
+        if (infoActive == true) {
+          // clicked a node while previous info block displayed
+          d3.selectAll("input").remove();
+          d3.select("#info").selectAll("*").remove();
+          d3.select("#info").style("opacity", 0);
+        }
+        d3.select("#info").style("opacity", 1);  // Display edit div
+
+        var div = d3.select("#info")  // Selet div for appending
+        // LABEL
+        var labelText = div.append("p")
+          .text("Label: ");
+        var labelInput = labelText.append("input")
+          .attr({
+            'size': '15',
+            'type': 'text',
+            'value': d.label
+          });
+
+        // PREFIX
+        var prefixText = div.append("p")
+            .text("Prefix: ");
+        var prefixInput = prefixText.append("input")
+          .attr({
+            'size': '15',
+            'type': 'text',
+            'value': d.prefix
+          });
+        //TYPE
+        var typeText = div.append("p")
+          .text("Type: ");
+        var typeInput = typeText.append("input")
+          .attr({
+              'size':   15,
+              'type':  'text',
+              'value':  d.type
+            });
+       //console.log("labelInput: " +labelInput.node().value);
+       //---- UPDATE BUTTON -----------------------------------------------------//
+        var button = div.append("button")
+          .text("Update/Hide")
+          .on("click", function() {
+              d3.select("#nodeText" + i)
+                .text(function(d) {
+                  return (d.label = labelInput.node().value); })
+              ;
+              // Change class of circle to match TYPE
+              d3.select("#circle" + i)
+                 //Hang head in shame for this horrible kludge. Make this smarter.
+                 //  detect exist class.If changed: Remove existing, update to new
+                .classed("string", false)  // remove the class
+                .classed("uri", false)  // remove the class
+                .classed(typeInput.node().value.toLowerCase(), true)
+              ;
+              d3.select("#prefixText" + i)
+                .text(function(d) {return (d.prefix = prefixInput.node().value); });
+              var foo = d3.select("#typeText" + i)
+                .text(function(d) {return (d.type = typeInput.node().value); });
+              // Clean up the info window
+              d3.select("#info").selectAll("*").remove();
+              d3.select("#info").style("opacity", 0);
+            })
+
+        infoActive = true;
+      }); // End of dblClick
+    //---- END NODE CLICK -------------------------------------------------------//
+    // dx sets how close to the node the label appears
+    // Need unique ID for each nodeText value in order to update it from the info window
+    nodes.append("text")
+      .attr({
+        'class':       function(d){return 'nodeText'},
+        'id':          function(d, i) {return("nodeText"+i) ; },
+        'text-anchor': 'middle',
+        'class':        'nodeLabel'
+      })
+      .text(function(d) { return d.label; }) ;
+
+    // Create unique IDS for the PREFIX and TYPE text for updating from the info boxE
+    nodes.append("prefixText")
+      .attr("id", function(d, i) {return("prefixText"+i) ; });
+    nodes.append("typeText")
+      .attr("id", function(d, i) {return("typeText"+i) ; });
 
 // Def for background of link text as a Filter effect as per:
 // https://www.w3.org/TR/SVG/filters.html
@@ -132,146 +241,6 @@ linkTextBack.append('feComposite')
   .attr("in", "SourceGraphic");
 
 */
-/*TW
-var drag = force.drag()
-  .on("dragstart", dragstart);
-*/
-
-// Relationship lines
-var edges = svg.selectAll("line")
-  .data(edgesData)
-  .enter()
-  .append("line")
-  .attr({
-    'id': function(d,i){return 'edge'+i},
-    'class': 'edge',
-    'marker-end': 'url(#arrowhead)'
-  })
-  .on('mouseover', function(d){
-    var nodeSelection = d3.select(this).style({opacity:'0.5'});
-   })
-  .on('mouseout', function(d){
-    var nodeSelection= d3.select(this).style({opacity:'1.0',})
-  });
-
-var nodes = svg.selectAll("g.node")
-  .data(nodesData)
-  .enter()
-  .append("g")
-  .attr("class", "node")
-  //.on("dblclick", dblclick)
-  // .call(drag);
-  .call(force.drag)
-  ;
-
-nodes.append("circle")
-  .attr("r", nodeRadius)
-  .attr("id", function(d, i) {return("circle"+i) ; })  // ID used to update class
-  .attr("class", function(d,i){
-    if (d.type == "STRING"){ return "string";}
-    else if (d.type == "URI"){ return "uri"; }
-    else {return "unspec";}
-  })
-
-  // Mousover Node - highlight node by fading the node colour during mouseover
-  .on('mouseover', function(d){
-    //var nodeSelection = d3.select(this).style({opacity:'0.5'});
-    var nodeSelection = d3.select(this).attr({'r':nodeRadius+5});
-  })
-
-  //Mouseout Node  - bring node back to full colour
-  .on('mouseout', function(d){
-    //  var nodeSelection= d3.select(this).style({opacity:'1.0',})
-    var nodeSelection = d3.select(this).attr({'r':nodeRadius});
-  })
-  //---- Double CLICK NODE TO EDIT ---------------------------------------------------//
-  // .on("click", function(d, i){  // was prev. single click
-  .on("dblclick", function(d, i){
-    console.log("clicked");
-    var self = this;
-
-    if (infoActive == true) {
-      // clicked a node while previous info block displayed
-      d3.selectAll("input").remove();
-      d3.select("#info").selectAll("*").remove();
-      d3.select("#info").style("opacity", 0);
-    }
-    d3.select("#info").style("opacity", 1);  // Display edit div
-
-    var div = d3.select("#info")  // Selet div for appending
-    // LABEL
-    var labelText = div.append("p")
-      .text("Label: ");
-    var labelInput = labelText.append("input")
-      .attr({
-        'size': '15',
-        'type': 'text',
-        'value': d.label
-      });
-
-    // PREFIX
-    var prefixText = div.append("p")
-        .text("Prefix: ");
-    var prefixInput = prefixText.append("input")
-      .attr({
-        'size': '15',
-        'type': 'text',
-        'value': d.prefix
-      });
-    //TYPE
-    var typeText = div.append("p")
-      .text("Type: ");
-    var typeInput = typeText.append("input")
-      .attr({
-          'size':   15,
-          'type':  'text',
-          'value':  d.type
-        });
-   //console.log("labelInput: " +labelInput.node().value);
-   //---- UPDATE BUTTON -----------------------------------------------------//
-    var button = div.append("button")
-      .text("Update/Hide")
-      .on("click", function() {
-          d3.select("#nodeText" + i)
-            .text(function(d) {
-              return (d.label = labelInput.node().value); })
-          ;
-          // Change class of circle to match TYPE
-          d3.select("#circle" + i)
-             //Hang head in shame for this horrible kludge. Make this smarter.
-             //  detect exist class.If changed: Remove existing, update to new
-            .classed("string", false)  // remove the class
-            .classed("uri", false)  // remove the class
-            .classed(typeInput.node().value.toLowerCase(), true)
-          ;
-          d3.select("#prefixText" + i)
-            .text(function(d) {return (d.prefix = prefixInput.node().value); });
-          var foo = d3.select("#typeText" + i)
-            .text(function(d) {return (d.type = typeInput.node().value); });
-          // Clean up the info window
-          d3.select("#info").selectAll("*").remove();
-          d3.select("#info").style("opacity", 0);
-        })
-
-    infoActive = true;
-  }); // End of dblClick
-//---- END NODE CLICK -------------------------------------------------------//
-// dx sets how close to the node the label appears
-// Need unique ID for each nodeText value in order to update it from the info window
-nodes.append("text")
-  .attr({
-    'class':       function(d){return 'nodeText'},
-    'id':          function(d, i) {return("nodeText"+i) ; },
-    'text-anchor': 'middle',
-    'class':        'nodeLabel'
-  })
-  .text(function(d) { return d.label; }) ;
-
-// Create unique IDS for the PREFIX and TYPE text for updating from the info boxE
-nodes.append("prefixText")
-  .attr("id", function(d, i) {return("prefixText"+i) ; });
-nodes.append("typeText")
-  .attr("id", function(d, i) {return("typeText"+i) ; });
 
 
 var edgepaths = svg.selectAll(".edgepath")
@@ -304,6 +273,34 @@ edgelabels.append('textPath')
   .style("pointer-events", "none")
   .text(function(d,i){return d.label});
 
+
+
+  /* Arrow for Link
+     Must have id:String for display
+     refX: increase to back the arrow away from the target node
+     markerHeight: Arrow height
+     markerWidth: Arrow Width
+  */
+svg.append('defs')
+  //.data(["arrowhead"])      // Different link/path types can be defined here
+  //.enter()
+  .append("marker")    // Append arrow marker
+    //TODO Move size and colouring to CSS
+    .attr({'id':'arrowhead',
+      'viewBox': '0 -5 10 10',
+      'refX':    nodeRadius+25,
+      'refY':    0,
+      'fill':    'black',
+      'stroke':  'white',
+      'markerWidth':  2.5,
+      'markerHeight': 2.5,
+      'orient': 'auto'
+    })
+    .append("svg:path")
+      .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+      .attr('fill', 'black')
+      .attr('stroke','black');  // move to CSS
+
 force.on("tick", function() {
   edges.attr("x1", function(d) {return d.source.x; })
     .attr("y1", function(d) {return d.source.y; })
@@ -317,30 +314,20 @@ force.on("tick", function() {
   edgepaths.attr('d', function(d) { var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
     //console.log(d)
     return path});
-
-    edgelabels.attr('transform',function(d,i){
-        if (d.target.x<d.source.x){
+  edgelabels.attr('transform',function(d,i){
+    if (d.target.x<d.source.x){
 //ERROR HERE. look into this. and this.node()
-           // console.log("getBBox: " + this.getBBox())
+
+// console.log("getBBox: " + this.getBBox())
 /*
             bbox = this.getBBox();
-            bbox = edgelabels.node().getBBox();
             rx = bbox.x+bbox.width/2;
             ry = bbox.y+bbox.height/2;
             return 'rotate(180 '+rx+' '+ry+')';
 */
-            }
-        else {
-            return 'rotate(0)';
-            }
-    });
+    }
+    else {
+      return 'rotate(0)';
+    }
+  });
 });
-
-
-// Set the "fixed" property of the dragged node to TRUE when a dragstart event is initiated,
-//   - removes "forces" from acting on that node and changing its position.
-/*
-function dragstart(d) {
-    d3.select(this).classed("fixed", d.fixed = true);
-}
-*/
