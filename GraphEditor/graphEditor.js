d9eccc96207ae184fa9e7c6200761bdc578440b8
Johannes Ulander <johannes.ulander@gmail.com>
2018-02-18 22:50:37 +0100

    Updated modal window

--- GraphEditor/graphEditor.js
+++ GraphEditor/graphEditor.js
@@ -876,27 +876,28 @@ function setLoadFiles(){
 
 function undo() {
     // Get the modal
-var modal = document.getElementById('myModal');
+    let modal = document.getElementById('myModal');
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+    // // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
     modal.style.display = "block";
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-    alert("tjena")
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
         modal.style.display = "none";
     }
-}
 
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+}
 
 function saveState(graph){
     // Backup the last state
